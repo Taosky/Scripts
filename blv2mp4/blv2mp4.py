7f34c8c02d3105238d31ea171d61e006847eeb15
Taosky <t@firefoxcn.net>
2017-08-26 16:50:52 +0800

    加入参数选项，对ffmpeg路径进行检测

--- blv2mp4/blv2mp4.py
+++ blv2mp4/blv2mp4.py
@@ -1,7 +1,39 @@
 import sys
 import os
 import shutil
-local_dir = str(sys.argv[1])
+import getopt
+
+# 本地视频目录
+local_dir = ''
+
+# ffmpeg路径
+ffmpeg = ''
+
+
+# 没有参数提示并退出
+if not sys.argv[1:]:
+    print('blv2mp4.py -i <inputpath> -f <ffmpegpath>')
+    sys.exit(2)
+
+# 获取参数
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hi:f:", ["input=", "ffmpeg="])
+
+except getopt.GetoptError:
+    print('blv2mp4.py -i <inputpath> -f <ffmpegpath>')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-h':
+        print('blv2mp4.py -i <inputpath> -f <ffmpegpath>')
+        sys.exit()
+    elif opt in ("-i", "--input"):
+        local_dir = arg
+    elif opt in ("-f", "--ffmpeg"):
+        ffmpeg = arg
+
+if ffmpeg.split(os.path.sep)[-1] != 'ffmpeg.exe' and ffmpeg.split(os.path.sep)[-1] != 'ffmpeg':
+    sys.exit('\nERROR: ffmpeg路径不正确\n')
 
 bli_info = dict()
 
@@ -18,7 +50,7 @@ for p_dir in os.listdir(local_dir):
                 for video in os.listdir(video_dir_path):
                     video_path = os.path.join(video_dir_path, video)
                     if not os.path.isdir(video_path) and (
-                            video.split('.')[-1] == 'flv' or video.split('.')[-1][0] == 'blv'):
+                                    video.split('.')[-1] == 'flv' or video.split('.')[-1] == 'blv'):
                         bli_info[p_dir]['video'].append(video_path)
 
             elif video_dir == 'danmaku.xml':
@@ -30,15 +62,16 @@ for part in bli_info:
         for line in bli_info[part]['video']:
             ff_line = "file '%s'" % line.strip() + '\n'
             f.write(ff_line)
-    command = 'ffmpeg -f concat -safe 0 -i ff.txt -c copy %s.mp4' % part_path
+    command = ffmpeg + ' -f concat -safe 0 -i ff.txt -c copy %s.mp4' % part_path
+
     print(command)
     os.system(command=command)
+
     try:
         shutil.copy(bli_info[part]['danmaku'], part_path + '.xml')
     except KeyError:
-        print('弹幕文件不存在...\n')
+        print('\nWarning: 弹幕文件不存在...\n')
 
     shutil.move(part_path, os.path.join(os.path.join(local_dir, 'source'), part))
 
 wait = input('完成！任意键退出')
-
